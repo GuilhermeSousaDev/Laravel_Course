064a9dd4228ff2694156fa09794e0cc5442d119a
GuilhermeSousaDev <a.guilhermerocha250@gmail.com>
2023-07-17 16:35:39 -0300

    Create an Event with Store Route

--- example-app/resources/views/layouts/main.blade.php
+++ example-app/resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="/styles/style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
 
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
 
+Route::post('/events', [EventController::class, 'store']);
+
 Route::view('/contact', 'contact');
