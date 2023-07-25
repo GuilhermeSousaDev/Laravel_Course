e09f1d6b9f50e15302b4fab750145aa8f8921f13
GuilhermeSousaDev <a.guilhermerocha250@gmail.com>
2023-07-25 16:48:00 -0300

    Search an Event

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -6,13 +6,17 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
-    <h2>Próximos Eventos</h2>
-    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @if($search)
+        <h2>Searching for: {{ $search }}</h2>
+    @else
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
@@ -22,13 +26,20 @@
                 <img src="/img/banner.jpg" alt="{{ $event->title }}">
             @endif
             <div class="card-body">
-                <p class="card-date">10/09/2020</p>
+                <p class="card-date">
+                    {{ date('d/m/Y', strtotime($event->date)) }}
+                </p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
-                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                <a href="/event/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
+        @if(count($events) == 0)
+            <p>There are no events with title: {{ $search }}</p>
+        @elseif(count($events) == 0)
+            <p>There are no events yet</p>
+        @endif
     </div>
 </div>
 
