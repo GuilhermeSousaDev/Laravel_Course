dcbb4ea7415860b153e6cf356978d66adb6faad0
GuilhermeSousaDev <a.guilhermerocha250@gmail.com>
2023-07-31 15:52:05 -0300

    Show the Event Owner in Event Info

--- example-app/app/Http/Controllers/EventController.php
+++ example-app/app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -33,9 +34,14 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
         return view(
             'events.show',
-            ['event' => $event],
+            [
+                'event' => $event,
+                'eventOwner' => $eventOwner,
+            ],
         );
 
     }
